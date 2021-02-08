f7ff47bbcf3583aa5da04b73f9e4ab7978fbb4a1
Zihadkhan101 <78749780+Zihadkhan101@users.noreply.github.com>
2021-02-08 22:41:22 +0600

    Add files via upload

---
+++ Isotope jquery Plugin/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    // init Isotope
+// var $grid = $('.portfolio-item').isotope({
+//     // options
+//   });
+//   // filter items on button click
+//   $('.portfolio-menu').on( 'click', 'li', function() {
+//     var filterValue = $(this).attr('data-filter');
+//     $grid.isotope({ filter: filterValue });
+//   });
+
+});
